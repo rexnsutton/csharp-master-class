fb69322917cfed2e8accecd7ba5865dcd823b2fd
Student Name <rexsutton2@gmail.cogit>
2020-11-04 16:24:10 -0600

    working on fields lesson

---
+++ fields/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
