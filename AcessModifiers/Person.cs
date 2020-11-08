54ab5bdc936d7aafdbd9f2bab1fe49d994e110a1
Student Name <rexsutton2@gmail.cogit>
2020-11-08 12:19:03 -0600

    finished lesson on access modifiers

--- AcessModifiers/Person.cs
+++ AcessModifiers/Person.cs
@@ -3,8 +3,30 @@ namespace AcessModifiers
 {
     public class Person
     {
-        public Person()
+
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
         {
+            _birthdate = birthdate;
+
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+
+        }
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+                var person = new Person();
+                person.SetBirthdate(new DateTime(1912, 1, 1));
+                Console.WriteLine(person.GetBirthdate());
+               
+            }
         }
     }
 }
